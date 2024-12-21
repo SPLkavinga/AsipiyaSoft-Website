0ced1b71bbe178978541c651441d9570c6742066
SPLkavinga <lkspathirana@gmail.com>
2024-12-21 11:38:16 +0530

    i have update the ui of landingpage in admin side

--- Frontend/src/Admin/Landingpage.jsx
+++ Frontend/src/Admin/Landingpage.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 function Landingpage() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-500 via-purple-50 to-purple-500">
-        <Link className="px-5 py-3 mb-4 text-center text-white bg-black rounded" to="/AddCareers">ADD CAREERS</Link>
-        <Link className="px-5 py-3 text-center text-white bg-black rounded" to="/ShowCareer">SEE CAREERS</Link>
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-purple-500 via-purple-50 to-purple-500">
+      <div className="flex flex-col items-center justify-center bg-black w-[400px] h-[500px]">
+        {/* First Button */}
+        <Link className="block px-5 py-3 mb-4 text-white bg-purple-400 rounded hover:bg-purple-300" to="/AddCareers">
+          ADD CAREERS
+        </Link>
+        {/* Second Button */}
+        <Link className="block px-5 py-3 text-white bg-purple-400 rounded hover:bg-purple-300" to="/ShowCareer">
+          SEE CAREERS
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
 
